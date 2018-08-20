4524bacb52de6c6f84cf82e3584c43b1a3e0cef4
wip727 <wip727@gmail.com>
2018-08-19 22:32:48 -0400

    Update index.js
    add pusher cluster to env

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ var app = express();
 var pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
-  secret: process.env.PUSHER_APP_SECRET
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_CLUSTER
 });
 
 streamer.pusher = pusher;
